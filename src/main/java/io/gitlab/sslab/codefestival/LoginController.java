be4058bd086172eda65547bb142e49ecff04967f
aerain <j40f893@gmail.com>
2018-11-04 15:11:56 +0900

    utf-8

--- src/main/java/io/gitlab/sslab/codefestival/LoginController.java
+++ src/main/java/io/gitlab/sslab/codefestival/LoginController.java
@@ -1,9 +1,11 @@
 package io.gitlab.sslab.codefestival;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+@Controller
 public class LoginController {
     @RequestMapping(value="/users")
     @ResponseBody
--- src/main/java/io/gitlab/sslab/codefestival/ThymeleafConfig.java
+++ src/main/java/io/gitlab/sslab/codefestival/ThymeleafConfig.java
@@ -1,21 +1,40 @@
 package io.gitlab.sslab.codefestival;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
 
 @EnableAutoConfiguration
+@EnableWebMvc
 @Configuration
-public class ThymeleafConfig {
+@ComponentScan(basePackages = "io.gitlab.sslab.codefestival")
+public class ThymeleafConfig implements WebMvcConfigurer, ApplicationContextAware {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
     @Bean
     public SpringResourceTemplateResolver templateResolver() {
         SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
         templateResolver.setCacheable(false);
         templateResolver.setPrefix("classpath:/views/");
         templateResolver.setSuffix(".html");
+        templateResolver.setCharacterEncoding("UTF-8");
         return templateResolver;
     }
 
@@ -30,7 +49,7 @@ public class ThymeleafConfig {
     public ThymeleafViewResolver viewResolver() {
         ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
         viewResolver.setTemplateEngine(templateEngine());
-        viewResolver.setOrder(1);
+        viewResolver.setCharacterEncoding("UTF-8");
         return viewResolver;
     }
 }
